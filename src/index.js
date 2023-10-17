808974eb6eb173bb0ec38809d4e14578759ccca5
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-16 20:22:41 -0700

    prework

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ const form = document.querySelector(`form`)
+ form.addEventListener(`submit`,(ev) =>{
+  ev.preventDefault()
+  taskAdder(ev.target.new_task_description.value)
+ })
 });
+
+function taskAdder(task){
+  let listitem= document.createElement(`li`)
+  let p = document.createElement(`p`)
+  p.textContent=` ${task}`
+  let btn = document.createElement(`button`)
+  btn.textContent=`X`
+  btn.addEventListener(`click`,deleteTask)
+  p.appendChild(btn)
+  listitem.appendChild(p)
+  document.querySelector(`#tasks`).appendChild(listitem)
+}
+function deleteTask(ev){
+  ev.target.parentNode.remove()
+}
