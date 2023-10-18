3015d66d1c5c16d3478191d55f2c074b752a338d
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-18 12:07:14 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  function fetchBooks() {
+    // To pass the tests, don't forget to return your fetch!
+    return fetch('https://anapioficeandfire.com/api/books')
+    .then((response)=> response.json())
+    .then((json) => renderBooks(json)) 
+  
+  }
+  
+  function renderBooks(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks();
+  });
  const form = document.querySelector(`form`)
  form.addEventListener(`submit`,(ev) =>{
   ev.preventDefault()
